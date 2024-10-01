7692ffcac6d32e927be573d95be350652729cb40
Eya-Guirat <guirateya8@gmail.com>
2024-10-01 21:49:47 +0100

    prosit3

--- src/Zoo.java
+++ src/Zoo.java
@@ -3,7 +3,7 @@ public class Zoo {
     Animal [] animals;
     String name;
     String city;
-     int nbrCages;
+    static int nbrCages=25;
 
     public Zoo(Animal[] animals, String name, String city,int nbrCages) {
         this.animals = new Animal[nbrCages];
@@ -26,4 +26,84 @@ public class Zoo {
                 ", nbrCages=" + nbrCages +
                 '}';
     }
+
+    public boolean addAnimal(Animal animal){
+        for (int i = 0; i < nbrCages; i++) {
+            if (animals[i]!=null && animal.name.equals(animals[i].name)  ) {
+                return false;
+            } else if (animals[i] == null) {
+                animals[i] = animal;
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+    public void displayAnimals(){
+        for (int i = 0; i < nbrCages; i++) {
+            if(animals[i] != null){
+                System.out.println(animals[i].toString());
+            }
+        }
+    }
+
+    public int searchAnimal(Animal animal){
+        for (int i = 0; i < nbrCages; i++) {
+            if (animals[i]==animal && animals[i].name.equals(animal.name)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+
+    public boolean removeAnimal(Animal animal){
+        for (int i = 0; i < nbrCages; i++) {
+            if (animals[i]==animal ) {
+                animals[i] = null;
+                System.out.println("animal removed");
+                for (int j = i ; j < nbrCages -1; j++) {
+                    animals[j] = animals[j+1];
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean isZooFull(){
+        int nbAnimals =0;
+        for (int i = 0; i < nbrCages; i++) {
+            if (animals[i]!=null) {
+                nbAnimals = nbAnimals + 1;
+            }
+        }
+        return (nbAnimals == nbrCages);
+    }
+
+
+    public static Zoo comparerZoo(Zoo z1, Zoo z2) {
+        int nbAnimalsZ1 = 0;
+        int nbAnimalsZ2 = 0;
+
+        for (int i = 0; i < z1.nbrCages; i++) {
+            if (z1.animals[i] != null) {
+                nbAnimalsZ1++;
+            }
+        }
+
+        for (int i = 0; i < z2.nbrCages; i++) {
+            if (z2.animals[i] != null) {
+                nbAnimalsZ2++;
+            }
+        }
+
+        return nbAnimalsZ1 > nbAnimalsZ2 ? z1 : z2;
+    }
+
+
+
+
 }
--- src/ZooManagement.java
+++ src/ZooManagement.java
@@ -16,21 +16,65 @@ public class ZooManagement {
 
         System.out.println(zooName + " comporte " + nbCages + " cages");
 
-        Animal animals[] = new Animal[25];
+        //prosit2
+        Animal[] animals = new Animal[25];
 
         Animal lion = new Animal("Felidae", "simba", 5, true);
+        Animal zebra = new Animal("Fam1", "flio", 5, true);
+        Animal elephant = new Animal("Fam2", "mlio", 5, true);
+        Animal tiger = new Animal("Felidae", "Shere Khan", 7, true);
+        Animal bear = new Animal("Ursidae", "Baloo", 10, true);
+
+        Animal zebra2 = new Animal("Fam1", "flio", 5, true);
 
         animals[0] = lion;
+        animals[1] = zebra;
+        animals[2] = elephant;
+        animals[3] = tiger;
+        animals[4] = bear;
 
         Zoo myZoo = new Zoo(animals, "safari", "australia", 18);
 
+        //prosit 3
+        myZoo.addAnimal(lion);
+        myZoo.addAnimal(zebra);
+        myZoo.addAnimal(elephant);
+        myZoo.addAnimal(tiger);
+        myZoo.addAnimal(bear);
+
         myZoo.displayZoo();
+        myZoo.displayAnimals();
 
-        System.out.println(myZoo);
-        System.out.println(myZoo.toString());
 
-       System.out.println(lion);git
-       System.out.println(lion.toString());
+        int indice = myZoo.searchAnimal(zebra);
+        System.out.println(indice);
+
+
+        indice = myZoo.searchAnimal(zebra2);
+        System.out.println(indice);
+
+        boolean remove = myZoo.removeAnimal(tiger);
+        myZoo.displayAnimals();
+
+        boolean isFull = myZoo.isZooFull();
+        System.out.println(isFull);
+
+        Zoo myZoo2 = new Zoo(animals, "safar", "South", 18);
+        myZoo2.addAnimal(bear);
+        myZoo2.addAnimal(lion);
+
+        Zoo comparer = Zoo.comparerZoo(myZoo, myZoo2);
+        System.out.println(comparer);
+
+
+//        System.out.println(myZoo);
+//        System.out.println(myZoo.toString());
+//
+//       System.out.println(lion);
+//       System.out.println(lion.toString());
+//
+//       myZoo.addAnimal(lion);
+//       System.out.println(myZoo);
 
     }
 }
